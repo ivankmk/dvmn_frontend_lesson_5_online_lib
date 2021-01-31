e7413c1513c2b0b0ad5a3b05bfdfe3c1312f00b0
IvanKMK <kumeyko@hotmail.com>
2021-01-31 20:00:30 +0100

    removed copy/paste of the columns

--- render_website.py
+++ render_website.py
@@ -6,6 +6,9 @@ from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 
 def rebuild():
+
+    elements_on_page = 10
+
     env = Environment(
         loader=FileSystemLoader('.'),
         autoescape=select_autoescape(['html', 'xml'])
@@ -16,18 +19,15 @@ def rebuild():
     with open("books_db.json", "r") as my_file:
         books_json = my_file.read()
 
-    all_books = list(chunked(json.loads(books_json), 10))
+    all_books = list(chunked(json.loads(books_json), elements_on_page))
 
     pages = len(all_books)
 
     for page_num, book_chunk in enumerate(all_books, 1):
-        col1, col2 = list(
-            chunked(book_chunk, math.ceil(len(book_chunk)/2))
-            )
-
+        books = list(chunked(book_chunk, math.ceil(len(book_chunk)/2)))
+        
         rendered_page = template.render(
-            col1=col1,
-            col2=col2,
+            books=books,
             page_num=page_num,
             pages=pages
             )
